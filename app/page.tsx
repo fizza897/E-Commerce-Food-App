b817ddba3c8666d2809fa625e77d9afaa5e6d360
fizza897 <fizzaazam6@gmail.com>
2024-09-23 13:26:39 +0500

    done

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
   <>
-    <div className="text-red-900 font-bold text-9xl">Hello world</div>
+    <div className="text-red-900">Hello world</div>
   
   </>
   );
